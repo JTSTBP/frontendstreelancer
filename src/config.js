4618d99f7c12d169086d98e99d28c142ef00b34b
GottemukkalaGayathri <gayathrigottemukkala123@gmail.com>
2025-07-04 11:34:14 +0530

    checking

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
     : process.env.REACT_APP_BACKEND_URL;
-
+console.log(API_BASE_URL,process.env.NODE_ENV)
 export default API_BASE_URL;
